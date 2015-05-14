85955fc603dd749a36de4580dc0ad147e6cdf259
Kyle Johnston <Kyle@KyleJohnston.net>
2015-05-14 13:24:48 -0500

    Version that was sent to Papadakis on 5/13/15 as a beta.

--- SalaryStatistics/SalaryStatistics/Form1.Designer.cs
+++ SalaryStatistics/SalaryStatistics/Form1.Designer.cs
@@ -52,32 +52,35 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(122, 9);
+            this.textBox1.Location = new System.Drawing.Point(125, 134);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(35, 20);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.TabIndex = 5;
             this.textBox1.Text = "2";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(122, 64);
+            this.textBox3.Location = new System.Drawing.Point(125, 184);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(35, 20);
-            this.textBox3.TabIndex = 4;
+            this.textBox3.TabIndex = 7;
             this.textBox3.Text = "1.03";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(122, 94);
+            this.textBox2.Location = new System.Drawing.Point(125, 210);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(35, 20);
-            this.textBox2.TabIndex = 6;
+            this.textBox2.TabIndex = 8;
             this.textBox2.Text = "1.015";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Location = new System.Drawing.Point(12, 137);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 13);
             this.label1.TabIndex = 7;
@@ -86,7 +89,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 71);
+            this.label2.Location = new System.Drawing.Point(12, 187);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(104, 13);
             this.label2.TabIndex = 8;
@@ -95,7 +98,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 97);
+            this.label3.Location = new System.Drawing.Point(12, 213);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(104, 13);
             this.label3.TabIndex = 9;
@@ -104,9 +107,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 134);
+            this.button1.Location = new System.Drawing.Point(169, 134);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(570, 122);
+            this.button1.Size = new System.Drawing.Size(534, 96);
             this.button1.TabIndex = 10;
             this.button1.Text = "RUN\r\nCALCULATION";
             this.button1.UseVisualStyleBackColor = true;
@@ -137,7 +140,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 45);
+            this.label4.Location = new System.Drawing.Point(12, 161);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(86, 13);
             this.label4.TabIndex = 16;
@@ -145,56 +148,60 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(122, 35);
+            this.textBox4.Location = new System.Drawing.Point(125, 158);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(35, 20);
-            this.textBox4.TabIndex = 15;
+            this.textBox4.TabIndex = 6;
             this.textBox4.Text = "3";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(166, 7);
+            this.button2.Location = new System.Drawing.Point(15, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 23);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "Import input 0";
+            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "UH Salary Data";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(166, 35);
+            this.button3.Location = new System.Drawing.Point(15, 40);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(119, 23);
-            this.button3.TabIndex = 18;
-            this.button3.Text = "Import input 1";
+            this.button3.Size = new System.Drawing.Size(145, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "[1] New Asst. Prof. Salaries";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(166, 65);
+            this.button4.Location = new System.Drawing.Point(15, 70);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(119, 23);
-            this.button4.TabIndex = 19;
-            this.button4.Text = "Import input 2";
+            this.button4.Size = new System.Drawing.Size(145, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "(2) Tier 1 Specalty Salaries";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(166, 97);
+            this.button5.Location = new System.Drawing.Point(15, 102);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(119, 23);
-            this.button5.TabIndex = 20;
-            this.button5.Text = "Import input 3";
+            this.button5.Size = new System.Drawing.Size(145, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "(3) UH Specalty Weights";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(291, 11);
+            this.label5.Location = new System.Drawing.Point(166, 17);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(115, 13);
             this.label5.TabIndex = 21;
@@ -203,7 +210,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(291, 40);
+            this.label6.Location = new System.Drawing.Point(166, 45);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(115, 13);
             this.label6.TabIndex = 22;
@@ -212,7 +219,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(291, 75);
+            this.label7.Location = new System.Drawing.Point(166, 75);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(115, 13);
             this.label7.TabIndex = 23;
@@ -221,7 +228,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(291, 107);
+            this.label8.Location = new System.Drawing.Point(166, 107);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(115, 13);
             this.label8.TabIndex = 24;
@@ -229,10 +236,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(12, 134);
+            this.button6.Location = new System.Drawing.Point(169, 134);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(193, 44);
-            this.button6.TabIndex = 25;
+            this.button6.TabIndex = 9;
             this.button6.Text = "Get Filters";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
@@ -241,7 +248,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(594, 271);
+            this.ClientSize = new System.Drawing.Size(715, 241);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
